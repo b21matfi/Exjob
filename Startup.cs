9e91ff32daaa2378350931c56dd319e17cae02d1
b21matfi <102650424+b21matfi@users.noreply.github.com>
2024-03-19 21:14:03 +0100

    Implemented compiler binding the textarea and iframe does not work

---
+++ Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Exjob;
+using Services;
+namespace Services
+{
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddSingleton<CompileService>();
+        }
+
+    }
+}

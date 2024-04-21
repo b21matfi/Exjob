79852d2cf780a4b83c17731019885e5244244dce
b21matfi <102650424+b21matfi@users.noreply.github.com>
2024-04-21 18:14:04 +0200

    Created a button that generates all the examples and runs them in a loop. Downloaded faker package so that it creates paragraphs

--- Program.cs
+++ Program.cs
@@ -2,7 +2,6 @@ using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Exjob;
 using Services;
-
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
